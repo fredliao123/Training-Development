7d51191bda9cb42c198c5aa82ab0f5f7d751c760
Fred <fred@ittybittyapps.com>
2021-01-06 15:39:23 +1100

    Thinking in compose

--- Compose/test-compose-app/app/src/main/java/com/example/testcompose/MainActivity.kt
+++ Compose/test-compose-app/app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -2,32 +2,63 @@ package com.example.testcompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testcompose.ui.TestComposeTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { 
-            Greeting(name = "Fred")
+        setContent {
+            newsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun newsStory() {
+    val image = imageResource(id = R.drawable.header)
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            val imageModifier = Modifier.preferredHeight(180.dp)
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(8.dp))
+            Image(bitmap = image, modifier = imageModifier, contentScale = ContentScale.Crop)
+
+            Text(
+                text = "A day in Shark FIn Cove, A day in Shark FIn Cove, A day in Shark FIn Cove, A day in Shark FIn Cove ",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(text = "Davenport California", style = typography.body2)
+            Text(text = "December 2018", style = typography.body2)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
-        Greeting("Android")
+        newsStory()
     }
 }
